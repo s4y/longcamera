e6c2a2bcadf1b4c2fc221ad3f2eb231faa3494b3
Sidney San Martín and Thomas Ballinger <hello+Sidnicious+thomasballinger@sidneysm.com>
2012-04-06 15:03:51 -0400

    "Patience" algorithm

--- filters/longcamera.js
+++ filters/longcamera.js
@@ -1,35 +1,34 @@
-var buffer = [];
-var finalImage;
+var finalImage, counts;
+
+var threshold = 30, patience = 50;
 
 self.addEventListener('message', function(e){
+	var r, g, b, distance, inputData = e.data.imageData.data;
 	if (!finalImage) {
 		finalImage = e.data.imageData;
+		counts = new Uint8Array(finalImage.data.length / 4);
 		self.postMessage({ name: 'readyForFrame' });
 		return;
 	}
-	buffer.push(e.data.imageData);
-	if (buffer.length > 20){
-		buffer.shift();
-	}
 	// var scores = scoreCtx.createImageData(200, 200);
-	for (var i = buffer[0].data.length - 1; i >= 0; i--) {
-		var counter = {};
-		var max = 0;
-		var candidate = 0;
-		for (var frame = 0, totalFrames = buffer.length; frame < totalFrames; frame++){
-			var value = buffer[frame].data[i];
-			if (value in counter){
-				counter[value]++;
-			} else {
-				counter[value] = 1;
-			}
-			if (counter[value] > max){
-				max = counter[value];
-				candidate = value;
-			}
+	for (var i = 0, l = inputData.length; i < l; i++) {
+		distance = 0;
+		r = inputData[i];
+		distance += Math.abs(r - finalImage.data[i++]);
+		g = inputData[i];
+		distance += Math.abs(g - finalImage.data[i++]);
+		b = inputData[i];
+		distance += Math.abs(b - finalImage.data[i++]);
+		// Just do this once for the alpha channel?
+		finalImage.data[i] = inputData[i];
+		if (distance < threshold) {
+			counts[i - 3 / 4] = 0;
+		} else if (++counts[i - 3 / 4] > patience) {
+			finalImage.data[i-3] = r;
+			finalImage.data[i-2] = g;
+			finalImage.data[i-1] = b;
+			counts[i - 3 / 4] = 0;
 		}
-		finalImage.data[i] = candidate;
-		// scores.data[i] = max * 255 / buffer.length;
 	}
 	self.postMessage({ name: 'outputFrame', imageData: finalImage });
 	self.postMessage({ name: 'readyForFrame' });
